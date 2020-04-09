c842dfa0646a550f62ed31eccf5d05a12fc6996b
OFFICE\felix.belanger <felix.belanger@gsoft.com>
2020-04-08 21:16:33 -0400

    fix prod build remove react router

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,37 +1,13 @@
 import React from "react"
-//import { Link} from "gatsby"
 import Layout from "../components/layout"
-import Home from "./home"
-import Inscrire from "./inscrire"
-import Impliquer from "./impliquer"
-import Contact from "./contact"
-import About from "./about"
-import NotFound from "./404"
+import SEO from "../components/seo"
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
 const IndexPage = () => (
-  <Router>  
-    <Layout>
-      <Switch>
-        <Route path="/" children={<Home />} />
-        
-        <Route path="/about" children={<About />} />
-        
-        <Route path="/inscrire" children={<Inscrire />} />
-        
-        <Route path="/impliquer" children={<Impliquer />} />
-        
-        <Route path="/contact" children={<Contact />}></Route>
-
-        <Route children={<NotFound />} />
-      </Switch>
-    </Layout>
-  </Router>
+  <Layout>
+    <SEO title="Accueil" />
+    <h1>L'Association des Aventuriers de Baden-Powell</h1>
+  </Layout>
 )
 
 export default IndexPage
