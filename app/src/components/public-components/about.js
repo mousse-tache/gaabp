4938271ecb0a30335c671a5a6d687ca1a13fed93
Félix Bélanger-Robillard <f.brobillard@gmail.com>
2020-06-06 12:10:44 -0400

    Feature/about section text (#3)
    * Add first api client
    
    * Minor tweaks on responsiveness and text for about section
    
    Co-authored-by: Félix Bélanger-Robillard <fbrobillard@gmail.com>

--- app/src/components/public-components/about.js
+++ app/src/components/public-components/about.js
@@ -4,6 +4,9 @@ const About = () => (
   <section>
     <div className="break" id="about"></div>
     <h1>Qui sommes-nous</h1>
+    <p>Nous sommes un mouvement scout traditionnel d’éducation pour et par les jeunes ayant comme objectif la formation de meilleurs citoyens.</p>
+    <p>L'Association des aventuriers de Baden-Powell (AABP) a été fondée en 2007 à la suite de la fusion de l'Association des Éclaireurs Baden-Powell AEBP, (1973 à 2007) et de l'Association Québécoise des Aventuriers de Brownsea, (1991 à 2007). C'est dans le but de sauvegarder et promouvoir certaines méthodes et approches pédagogiques du scoutisme et du guidisme de souche traditionnelle que celle-ci fut fondée. L'AABP rassemble aujourd'hui près de 1200 membres au Québec et au Nouveau-Brunswick. L'Association projette de s'implanter dans les autres provinces canadiennes et partout en Amérique du Nord. </p>
+    <p>La santé, la formation du caractère, la débrouillardise et l'esprit de service sont les cinq buts vers lesquels nous orientons toute l’approche éducative de notre association.</p>
   </section>
 )
 
