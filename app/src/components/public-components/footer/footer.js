cdb3107d3e52083a3d45661d45b47478571479fc
msieurmoustache <fbrobillard@gmail.com>
2020-06-06 13:21:24 -0400

    more header and footer css tweaks

--- app/src/components/public-components/footer/footer.js
+++ app/src/components/public-components/footer/footer.js
@@ -5,17 +5,13 @@ import Logo from "../../../images/Logo_AABP.gif"
 
 const Footer = () => (
   <footer> 
-          <div className="left-footer">
           <Link to="/">
             <img src={Logo} alt="Logo"/>
           </Link>
-          </div>
-          <div className="center-footer">
           <Link to="#about">
             Qui sommes-nous?
           </Link>
-          </div>
-          <div className="right-footer">         
+          <div>         
           <a href="https://carrickquebec.com/" target="_blank" rel="noopener noreferrer">Carrick (boutique)</a>
           </div>
   </footer>
