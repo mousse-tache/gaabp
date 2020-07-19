19c516e32287dcdecaae3f780ad23b0e6cbeddc6
msieurmoustache <fbrobillard@gmail.com>
2020-07-19 19:16:35 -0400

    site validation

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -13,6 +13,7 @@ const IndexPage = () => (
   <div>
     <Helmet>
           <meta name="description" content="L'Association des Aventuriers de Baden-Powell est une association de scoutisme traditionnel pour les 7 ans et plus."/>
+          <meta name="google-site-verification" content="Q2zhljR5H-cSJKdJBwFZaky-pEMiHM9NPK2pIgnVm6c" />
           <title>AABP | Scoutisme traditionnel</title>
           <html lang="fr" />
     </Helmet>
