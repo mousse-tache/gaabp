7762c0aac4a10aa9d9005f81ea0ea6a7f61aeb70
msieurmoustache <fbrobillard@gmail.com>
2020-09-20 12:49:56 -0400

    add back theme

--- app/src/components/public-components/layout.js
+++ app/src/components/public-components/layout.js
@@ -2,22 +2,43 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider  } from '@material-ui/core/styles';
 
 import Header from "./header/header"
 import Footer from "./footer/footer"
 import "../layout.css"
 
 const Layout = ({ children }) => {
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#d95764',
+        main: '#a3233a',
+        dark: '#6e0015',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#fff961',
+        main: '#edc62a',
+        dark: '#b69600',
+        contrastText: '#000',
+      },
+      root: {
+        textTransform: 'none',
+      }
+    },
+  });
   
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <CssBaseline />
       <div>
         <main>{children}</main>
         <Footer/>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
