8e0ec00670c442ce0bd8d42951e477fd97aa3ea7
msieurmoustache <fbrobillard@gmail.com>
2020-09-29 21:59:44 -0400

    fix flex

--- app/src/components/formation/formation.js
+++ app/src/components/formation/formation.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react"
 import Loading from "../loading/loading"
-import { Typography, Grid, Card, List, ListItem, Button, Fab } from '@material-ui/core';
+import { Card, List, ListItem, Button, Fab } from '@material-ui/core';
 import UserContext from "../../context/userContext";
 import "./formation.css";
 
@@ -16,8 +16,8 @@ const Formation = () => {
         <div className="membres-title">
             <div className="membres-title-element"><h3>Formation à distance 2020</h3></div>
         </div>
-        <Grid container spacing={3} > 
-            <Grid item xs={3}>
+        <div className="formation-main-container" container spacing={3} > 
+            <div className="formation-disponibles">
                 <Card className="formation-card preformatted">
                     <b>Formations disponibles</b>
                     <List>
@@ -53,8 +53,8 @@ const Formation = () => {
                         </ListItem>
                     </List>
                 </Card>
-            </Grid>
-            <Grid item xs={9}>
+            </div>
+            <div className="formation-main-message">
                 <Card className="formation-card preformatted">
                     <blockquote style={{lineHeight: "2rem"}}>
                         {"Bonjour à tous\, \n \n Cette année hors du commun nous aura permis de surmonter un nouveau défi à la formation; Un programme à distance fonctionnel sans avoir à faire de concessions sur notre programme de formation initial. \n Un travail colossal a été accompli et les formateurs n’attendent que des participants pour partager avec dynamisme leur savoir. \n Les formateurs communiqueront avec TOUS les participants avant le début de la formation pour revoir le cahier du participant avec eux et s'assurer de la compréhension de tous. \n \n Marianne De Garie \n Commissaire à la formation"}
@@ -63,8 +63,8 @@ const Formation = () => {
                     <Button variant="outlined" color="primary" target="_blank" href="https://forms.gle/Qn7mP9VBKoDYGbCMA" >Inscription</Button>
                     </div>
                 </Card>
-            </Grid>
-        </Grid>
+            </div>
+        </div>
     </div>
     )
 }
