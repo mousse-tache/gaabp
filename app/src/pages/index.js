3266d5ff9eb8b028f4192ceb7de3323f1442a433
Félix Bélanger-Robillard <felix.belanger@gsoft.com>
2021-05-28 09:27:11 -0400

    add logo spinner

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-
-import { Typography } from "@material-ui/core";
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Layout from "@aabp/components/public-components/Layout";
-import About from "@aabp/components/public-components/About";
-import Inscrire from "@aabp/components/public-components/Inscrire";
-import Impliquer from "@aabp/components/public-components/Impliquer";
-import Contact from "@aabp/components/public-components/Contact";
-import ImageCarousel from  "@aabp/components/public-components/ImageCarousel";
-import Covid19 from "@aabp/components/public-components/mesures-sanitaires/Covid19";
-
-import Logo from "@aabp/images/Logo_AABP.png";
+import LandingPage from "@aabp/components/public-components/LandingPage";
 
 const IndexPage = () => (
   <div>
@@ -25,18 +16,7 @@ const IndexPage = () => (
     </Helmet>
     <CssBaseline />
     <Layout>  
-      <section name="home" className="sitename anchor">
-        <Typography className="title-container" variant="h4" gutterBottom> 
-          <img className="morelinks hidden-logo" src={Logo} alt="Logo AABP"/>
-          <span>Association des aventuriers de Baden-Powell</span>
-        </Typography>
-        <Covid19 />
-        <ImageCarousel />
-      </section>
-      <About/>
-      <Inscrire />
-      <Impliquer />
-      <Contact />    
+      <LandingPage />   
     </Layout>
   </div>
 );
