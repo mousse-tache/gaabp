f263f4a136a8ca61bbab6e96fbe1a49dea952727
Félix Bélanger-Robillard <felix.belanger@gsoft.com>
2022-09-14 19:30:44 -0400

    inclusion des CEPs dans le calcul des coûts

--- app/src/components/recensement/calculateRecensementCost.js
+++ app/src/components/recensement/calculateRecensementCost.js
@@ -22,7 +22,7 @@ function CalculateCost(unitMembers, isUniteCadette = true) {
         return {details: {formedUsers:0, adultUsers:0, others:0, totalCount}, basePrice: null, totalPrice: 0};
     }
 
-    var formedUsers = unitMembers.filter(x => x.formations?.filter(y => y.dateConfirme && y.niveau?.name?.includes("BC")).length > 0).length;
+    var formedUsers = unitMembers.filter(x => x.formations?.filter(y => y.dateConfirme && (y.niveau?.name?.includes("BC1") || y.niveau?.name?.includes("BC2") || y.niveau?.name?.includes("BC3") || y.niveau?.name?.includes("CEP"))).length > 0).length;
     var adultUsers = unitMembers.filter(x => x.formations?.filter(y => y.dateConfirme).length == 0 && x.nominations.type !== NominationTypes.Membre).length;
 
     return { details: {formedUsers, adultUsers, others: totalCount-formedUsers-adultUsers, totalCount}, basePrice, totalPrice:(formedUsers)+((adultUsers)*25)+((totalCount-formedUsers-adultUsers)*basePrice)};
