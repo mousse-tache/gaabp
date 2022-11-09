378e2d729439ec35096c596b15c1a09e0779f8b1
Félix Bélanger-Robillard <felix.belanger@gsoft.com>
2022-11-09 18:08:36 -0500

    fix blatant error in permissions

--- api/src/security/permissions.js
+++ api/src/security/permissions.js
@@ -12,39 +12,39 @@ exports.Permissions = (userJwt, permission, unitId = null) => {
     }
     
     const isGeneralCommissionner = () => {
-        return authedUser.nominations.filter(x => x.type.includes("VPScout") && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("VPScout") && !x.ed).length > 0;
     };
 
     const isManagementVicepResident = () => {
-        return authedUser.nominations.filter(x => x.type.includes("VPGestion") && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("VPGestion") && !x.ed).length > 0;
     };
 
     const isCommissionner = () => {
-        return authedUser.nominations.filter(x => x.type.includes("Commissaire") && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("Commissaire") && !x.ed).length > 0;
     };
 
     const isBranchCommissionner = () => {
-        return authedUser.nominations.filter(x => x.type.includes("Commissaire") && NominationTypes[x.type].includes("branche") && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("Commissaire") && NominationTypes[x.type].includes("branche") && !x.ed).length > 0;
     };
 
     const isGroupCommissionner = () => {
-        return  authedUser.nominations.filter(x => x.type.includes("Commissaire") && x.type.includes("Groupes") && !x.ed).length > 0;
+        return  user.nominations.filter(x => x.type.includes("Commissaire") && x.type.includes("Groupes") && !x.ed).length > 0;
     };
 
     const isChief = () => {
         if(unitId) {
-            return authedUser.nominations.filter(x => x.type.includes("Chef") && x.unitId === unitId && !x.ed).length > 0;
+            return user.nominations.filter(x => x.type.includes("Chef") && x.unitId === unitId && !x.ed).length > 0;
         }
 
-        return authedUser.nominations.filter(x => x.type.includes("Chef") && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("Chef") && !x.ed).length > 0;
     };
     
     const isGroupChief = () => {
-        return authedUser.nominations.filter(x => x.type.includes("Chef") && !x.unitId && !x.ed).length > 0;
+        return user.nominations.filter(x => x.type.includes("Chef") && !x.unitId && !x.ed).length > 0;
     };
 
     const isFormateur = () => {
-        return authedUser.formations.filter(x => x.niveau.id === "32" && x.dateConfirmed).length > 0;
+        return user.formations.filter(x => x.niveau.id === "32" && x.dateConfirmed).length > 0;
     };
 
     switch(permission) {
