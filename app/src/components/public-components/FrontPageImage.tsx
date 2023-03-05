44307ef8a71cafcb8c2dbdbe875c4f7e8dbafc1b
Félix Bélanger-Robillard <felix.belanger@gsoft.com>
2023-03-05 11:10:33 -0500

    Replace mui switch by homemade toggle and change front image

---
+++ app/src/components/public-components/FrontPageImage.tsx
@@ -0,0 +1,18 @@
+import image1 from "@aabp/images/formation2022.jpeg";
+
+const FrontPageImage = () => {
+  return (
+    <div className="p-4 m-4">
+      <img
+        className="m-auto rounded-lg"
+        src={image1}
+        width="600"
+        height="338"
+        alt="Formation 2022"
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+export default FrontPageImage;
